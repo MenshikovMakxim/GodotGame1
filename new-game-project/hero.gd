e62ea56557b4f242793a5166d752342d544d5a2e
MenshikovMakxim <menshikov5254@gmail.com>
2025-09-02 12:51:01 +0300

    add different speeds

--- new-game-project/hero.gd
+++ new-game-project/hero.gd
@@ -1,5 +1,19 @@
 extends Node2D
 
+@export var speed=100
+
 func _process(delta: float) -> void:
-	self.position.x+=0.5
-	self.position.y+=0.5
+	#self.position.x+=0.2
+	pass
+	
+func _input(event: InputEvent) -> void:
+	print(event)
+	if event is InputEventKey:
+		if event.keycode==KEY_UP:
+			self.position.y-=speed
+		if event.keycode==KEY_LEFT:
+			self.position.x-=speed
+		if event.keycode==KEY_DOWN:
+			self.position.y+=speed
+		if event.keycode==KEY_RIGHT:
+			self.position.x+=speed
