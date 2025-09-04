6af65b6dc56f9f56f71552b0da1a9ba88ef31539
MenshikovMakxim <menshikov5254@gmail.com>
2025-09-04 16:35:07 +0300

    added coins

--- new-game-project/main.gd
+++ new-game-project/main.gd
@@ -6,7 +6,10 @@ func _on_check_button_button_down() -> void:
 	$Icon.position.x+=10
 	
 
-
 func _on_timer_timeout() -> void:
 	print("tick")
+	var newCoin = preload("res://Coin.tscn").instantiate()
+	newCoin.position.x = randi_range(0,800)
+	newCoin.position.y = randi_range(0,600)
+	self.add_child(newCoin)
 	pass # Replace with function body.
