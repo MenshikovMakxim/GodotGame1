228632e5812105791cc05085392d0b7c1edcfc70
MenshikovMakxim <menshikov5254@gmail.com>
2025-09-04 17:26:11 +0300

    added AI-Hero

--- new-game-project/coin.gd
+++ new-game-project/coin.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.name.begins_with("Hero2"):
-		self.get_parent().change_score(1)
+	if area.name.begins_with("Hero"):
+		#self.get_parent().change_score(1)
+		area.change_score(1)
 		queue_free()
 	pass # Replace with function body.
--- new-game-project/hero.gd
+++ new-game-project/hero.gd
@@ -1,12 +1,38 @@
 extends Node2D
 
 @export var speed=100
+@export var isAI = false
+var dirX = 1
+var dirY = 1
+var timeOfmovement = 0
+var timeOfDirChange = 3
+
+var score = 0
+
+func change_score(delta):
+	score+=delta
+	$Score.text = str(score)
 
 func _process(delta: float) -> void:
 	#self.position.x+=0.2
+	if isAI:
+		self.position.x+=speed*delta*dirX
+		self.position.y+=speed*delta*dirY
+		if self.position.x>=1092 or self.position.x<=60:
+			dirX = -dirX
+		if self.position.y>=588 or self.position.y<=60:
+			dirY = -dirY
+			
+		timeOfDirChange+=delta
+		if timeOfmovement>=timeOfDirChange:
+			timeOfmovement=0
+			dirX = randf_range(-1,1)
+			dirY = randf_range(-1,1)
 	pass
 	
 func _input(event: InputEvent) -> void:
+	if isAI:
+		return
 	print(event)
 	if event is InputEventKey:
 		if event.keycode==KEY_UP:
